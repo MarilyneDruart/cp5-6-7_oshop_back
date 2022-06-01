01f0ca6c83a7389e2ad7cb3c2944bfc71d15a5fa
MarilyneDruart <marilyne.druart@outlook.com>
2022-06-01 18:29:36 +0200

    ajout des liens dans la nav

--- app/views/partials/nav.tpl.php
+++ app/views/partials/nav.tpl.php
@@ -1,6 +1,10 @@
 <nav>
     <ul>
         <li><a href="<?= $router->generate('main-home') ?>">Accueil</a></li>
+        <li><a href="<?= $router->generate('category-list') ?>">Liste des catégories</a></li>
+        <li><a href="<?= $router->generate('category-add') ?>">Ajout de catégories</a></li>
+        <li><a href="<?= $router->generate('products-list') ?>">Liste des produits</a></li>
+        <li><a href="<?= $router->generate('products-add') ?>">Ajout de catégories</a></li>
         <li><a href="<?= md5(time()) ?>">404</a></li>
     </ul>
 </nav>
