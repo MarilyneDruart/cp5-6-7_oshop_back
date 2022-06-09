f5d13bf8d0f2d167beced8ad5896a32f113a70cd
MarilyneDruart <marilyne.druart@outlook.com>
2022-06-09 20:23:16 +0200

    récupération des catégories en fonction de leur ID home_order

--- app/views/shop/home.tpl.php
+++ app/views/shop/home.tpl.php
@@ -9,12 +9,22 @@
             <div class="form-group">
                 <label for="emplacement1">Emplacement #1</label>
                 <select class="form-control" id="emplacement1" name="emplacement[]">
-                    <option value="">choisissez :</option>
-                    <option value="1" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 1) ? 'selected' : '' ?>>Détente</option>
-                    <option value="2" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 2) ? 'selected' : '' ?>>Au travail</option>
-                    <option value="3" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 3) ? 'selected' : '' ?>>Cérémonie</option>
-                    <option value="4" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 4) ? 'selected' : '' ?>>Sortir</option>
-                    <option value="5" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 5) ? 'selected' : '' ?>>Vintage</option>
+
+                    <?php foreach ($categories as $key => $category) : ?>
+
+                        <?php if ($key === 0) : ?>
+                            <option>--</option>
+                        <?php endif; ?>
+                        
+                        <option value="<?= $category->getHomeOrder() ?>" <?= isset($_POST['category']) ? (
+                                $_POST['category'] == $category->getId() ? 'selected' : ''
+                            ) : (
+                                $category->getHomeOrder() == 1 ? 'selected' : ''
+                            ) ?>><?= $category->getName() ?>
+                        </option>
+                        
+                    <?php endforeach; ?>
+
                 </select>
             </div>
         </div>
@@ -22,12 +32,22 @@
             <div class="form-group">
                 <label for="emplacement2">Emplacement #2</label>
                 <select class="form-control" id="emplacement2" name="emplacement[]">
-                    <option value="">choisissez :</option>
-                    <option value="1" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 1) ? 'selected' : '' ?>>Détente</option>
-                    <option value="2" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 2) ? 'selected' : '' ?>>Au travail</option>
-                    <option value="3" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 3) ? 'selected' : '' ?>>Cérémonie</option>
-                    <option value="4" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 4) ? 'selected' : '' ?>>Sortir</option>
-                    <option value="5" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 5) ? 'selected' : '' ?>>Vintage</option>
+
+                    <?php foreach ($categories as $key => $category) : ?>
+
+                        <?php if ($key === 0) : ?>
+                            <option>--</option>
+                        <?php endif; ?>
+
+                        <option value="<?= $category->getHomeOrder() ?>" <?= isset($_POST['category']) ? (
+                                $_POST['category'] == $category->getId() ? 'selected' : ''
+                            ) : (
+                                $category->getHomeOrder() == 2 ? 'selected' : ''
+                            ) ?>><?= $category->getName() ?>
+                        </option>
+
+                        <?php endforeach; ?>
+
                 </select>
             </div>
         </div>
@@ -37,12 +57,22 @@
             <div class="form-group">
                 <label for="emplacement3">Emplacement #3</label>
                 <select class="form-control" id="emplacement3" name="emplacement[]">
-                    <option value="">choisissez :</option>
-                    <option value="1" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 1) ? 'selected' : '' ?>>Détente</option>
-                    <option value="2" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 2) ? 'selected' : '' ?>>Au travail</option>
-                    <option value="3" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 3) ? 'selected' : '' ?>>Cérémonie</option>
-                    <option value="4" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 4) ? 'selected' : '' ?>>Sortir</option>
-                    <option value="5" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 5) ? 'selected' : '' ?>>Vintage</option>
+                   
+                    <?php foreach ($categories as $key => $category) : ?>
+
+                        <?php if ($key === 0) : ?>
+                            <option>--</option>
+                        <?php endif; ?>
+
+                        <option value="<?= $category->getHomeOrder() ?>" <?= isset($_POST['category']) ? (
+                                $_POST['category'] == $category->getId() ? 'selected' : ''
+                            ) : (
+                                $category->getHomeOrder() == 3 ? 'selected' : ''
+                            ) ?>><?= $category->getName() ?>
+                        </option>
+
+                    <?php endforeach; ?>
+
                 </select>
             </div>
         </div>
@@ -50,12 +80,22 @@
             <div class="form-group">
                 <label for="emplacement4">Emplacement #4</label>
                 <select class="form-control" id="emplacement4" name="emplacement[]">
-                    <option value="">choisissez :</option>
-                    <option value="1" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 1) ? 'selected' : '' ?>>Détente</option>
-                    <option value="2" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 2) ? 'selected' : '' ?>>Au travail</option>
-                    <option value="3" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 3) ? 'selected' : '' ?>>Cérémonie</option>
-                    <option value="4" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 4) ? 'selected' : '' ?>>Sortir</option>
-                    <option value="5" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 5) ? 'selected' : '' ?>>Vintage</option>
+                   
+                    <?php foreach ($categories as $key => $category) : ?>
+
+                        <?php if ($key === 0) : ?>
+                            <option>--</option>
+                        <?php endif; ?>
+
+                        <option value="<?= $category->getHomeOrder() ?>" <?= isset($_POST['category']) ? (
+                                $_POST['category'] == $category->getId() ? 'selected' : ''
+                            ) : (
+                                $category->getHomeOrder() == 4 ? 'selected' : ''
+                            ) ?>><?= $category->getName() ?>
+                        </option>
+
+                    <?php endforeach; ?>
+
                 </select>
             </div>
         </div>
@@ -63,15 +103,25 @@
             <div class="form-group">
                 <label for="emplacement5">Emplacement #5</label>
                 <select class="form-control" id="emplacement5" name="emplacement[]">
-                    <option value="">choisissez :</option>
-                    <option value="1" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 1) ? 'selected' : '' ?>>Détente</option>
-                    <option value="2" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 2) ? 'selected' : '' ?>>Au travail</option>
-                    <option value="3" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 3) ? 'selected' : '' ?>>Cérémonie</option>
-                    <option value="4" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 4) ? 'selected' : '' ?>>Sortir</option>
-                    <option value="5" <?= (isset($_POST['emplacement[]']) && $_POST['emplacement[]'] == 5) ? 'selected' : '' ?>>Vintage</option>
+                   
+                <?php foreach ($categories as $key => $category) : ?>
+
+                    <?php if ($key === 0) : ?>
+                        <option>--</option>
+                    <?php endif; ?>
+
+                    <option value="<?= $category->getHomeOrder() ?>" <?= isset($_POST['category']) ? (
+                            $_POST['category'] == $category->getId() ? 'selected' : ''
+                        ) : (
+                            $category->getHomeOrder() == 5 ? 'selected' : ''
+                        ) ?>><?= $category->getName() ?>
+                    </option>
+
+                <?php endforeach; ?>
+
                 </select>
             </div>
         </div>
     </div>
     <button type="submit" class="btn btn-primary btn-block mt-5">Valider</button>
-</form>*rme;hà
+</form>
